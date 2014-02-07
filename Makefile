9dc41124e8e5c417ac5abe8f821d7d9161b0178b
root <root@bt.foo.org>
2014-02-07 15:25:05 +0530

    Lab 4 content

---
+++ Makefile
@@ -0,0 +1,150 @@
+.SUFFIXES: .cpp .hpp
+
+# Programs
+SHELL 	= bash
+CC     	= g++
+LD	= ld
+RM 	= rm
+ECHO	= /bin/echo
+CAT	= cat
+PRINTF	= printf
+SED	= sed
+DOXYGEN = doxygen
+AR      = ar
+######################################
+# Project Name (generate executable with this name)
+TARGET = cs296_19_exe
+LIBTARGET = cs296_19_exelib
+
+# Project Paths
+PROJECT_ROOT=./
+EXTERNAL_ROOT=$(PROJECT_ROOT)/external
+SRCDIR = $(PROJECT_ROOT)/src
+OBJDIR = $(PROJECT_ROOT)/myobjs
+BINDIR = $(PROJECT_ROOT)/mybins
+DOCDIR = $(PROJECT_ROOT)/doc
+LIBDIR = $(PROJECT_ROOT)/mylibs
+
+# Library Paths
+BOX2D_ROOT=$(EXTERNAL_ROOT)
+GLUI_ROOT=/usr
+GL_ROOT=/usr/include/
+
+#Libraries
+LIBS = -lBox2D -lglui -lglut -lGLU -lGL
+
+# Compiler and Linker flags
+CPPFLAGS =-g -O3 -Wall -fno-strict-aliasing
+CPPFLAGS+=-I $(BOX2D_ROOT)/include -I $(GLUI_ROOT)/include
+LDFLAGS+=-L $(BOX2D_ROOT)/lib -L $(GLUI_ROOT)/lib
+
+######################################
+SHARED_LIB = FALSE
+
+NO_COLOR=\e[0m
+OK_COLOR=\e[1;32m
+ERR_COLOR=\e[1;31m
+WARN_COLOR=\e[1;33m
+MESG_COLOR=\e[1;34m
+FILE_COLOR=\e[1;37m
+
+OK_STRING="[OK]"
+ERR_STRING="[ERRORS]"
+WARN_STRING="[WARNINGS]"
+OK_FMT="${OK_COLOR}%30s\n${NO_COLOR}"
+ERR_FMT="${ERR_COLOR}%30s\n${NO_COLOR}"
+WARN_FMT="${WARN_COLOR}%30s\n${NO_COLOR}"
+######################################
+
+SRCS := $(wildcard $(SRCDIR)/*.cpp)
+INCS := $(wildcard $(SRCDIR)/*.hpp)
+OBJS := $(SRCS:$(SRCDIR)/%.cpp=$(OBJDIR)/%.o)
+LIBOBJS := $(OBJS:$(OBJDIR)/main.o=)
+
+.PHONY: all setup doc clean distclean exe exelib
+
+all: setup
+
+setup:
+	@mkdir -p $(OBJDIR)
+	@mkdir -p $(LIBDIR)
+	@mkdir -p $(BINDIR)
+	@if (! test -d $(EXTERNAL_ROOT)/include/Box2D) || (! test -e $(EXTERNAL_ROOT)/lib/libBox2D.a); then \
+	cd $(EXTERNAL_ROOT)/src; \
+	tar xzf Box2D.tgz; \
+	mkdir -p Box2D/build296; \
+	cd Box2D/build296; \
+	cmake ../; \
+	make; \
+	make install; \
+	fi;
+
+exe: $(OBJS)
+	@$(PRINTF) "$(MESG_COLOR)Building executable:$(NO_COLOR) $(FILE_COLOR) %16s$(NO_COLOR)" "$(notdir $@)"
+	$(CC) -o $(BINDIR)/$(TARGET) $(LDFLAGS) $(OBJS) $(LIBS) 2> temp.log || touch temp.err
+	@if test -e temp.err; \
+	then $(PRINTF) $(ERR_FMT) $(ERR_STRING) && $(CAT) temp.log; \
+	elif test -s temp.log; \
+	then $(PRINTF) $(WARN_FMT) $(WARN_STRING) && $(CAT) temp.log; \
+	else $(PRINTF) $(OK_FMT) $(OK_STRING); \
+	fi;
+	@$(RM) -f temp.log temp.err
+
+$(LIBDIR)/libCS296test.a: $(OBJS)
+	@if [ "$(SHARED_LIB)" == "FALSE" ]; then \
+	$(PRINTF) "$(MESG_COLOR)Building static library:$(NO_COLOR) $(FILE_COLOR) %16s$(NO_COLOR)" "$(notdir $@)"; \
+	$(AR) -cvr $@ $(LIBOBJS); \
+	$(ECHO) -n "Static Library Built"; \
+	fi;
+
+$(LIBDIR)/libCS296test.so: $(OBJS)
+	@if [ "$(SHARED_LIB)" != "FALSE" ]; then \
+	$(PRINTF) "$(MESG_COLOR)Building dynamic library:$(NO_COLOR) $(FILE_COLOR) %16s$(NO_COLOR)" "$(notdir $@)"; \
+	$(CC) -shared -o $@ $(LIBOBJS); \
+	$(ECHO) -n "Dynamic Library Built"; \
+	fi;
+
+exelib: setup $(LIBDIR)/libCS296test.a $(LIBDIR)/libCS296test.so
+	@$(ECHO) -n "Linking Library and Building Executable... "
+	@if [ "$(SHARED_LIB)" == "FALSE" ]; then \
+	$(CC) -o $(BINDIR)/$(LIBTARGET) $(LDFLAGS) -L $(LIBDIR) $(OBJDIR)/main.o -l:libCS296test.a $(LIBS); else \
+	$(CC) -o $(BINDIR)/$(LIBTARGET) $(LDFLAGS) -L $(LIBDIR) $(OBJDIR)/main.o -l:libCS296test.so $(LIBS); \
+	fi;
+	@$(ECHO) -n "Executable Successfully Built!"
+
+-include $(OBJS:.o=.d)
+
+$(OBJS): $(OBJDIR)/%.o : $(SRCDIR)/%.cpp
+	@$(PRINTF) "$(MESG_COLOR)Compiling: $(NO_COLOR) $(FILE_COLOR) %25s$(NO_COLOR)" "$(notdir $<)"
+	$(CC) -fPIC $(CPPFLAGS) -c $< -o $@ -MD 2> temp.log || touch temp.err
+	@if test -e temp.err; \
+	then $(PRINTF) $(ERR_FMT) $(ERR_STRING) && $(CAT) temp.log; \
+	elif test -s temp.log; \
+	then $(PRINTF) $(WARN_FMT) $(WARN_STRING) && $(CAT) temp.log; \
+	else printf "${OK_COLOR}%30s\n${NO_COLOR}" "[OK]"; \
+	fi;
+	@$(RM) -f temp.log temp.err
+
+doc:
+	@$(ECHO) -n "Generating Doxygen Documentation ...  "
+	@$(RM) -rf doc/html
+	@$(DOXYGEN) $(DOCDIR)/Doxyfile 2 > /dev/null
+	@$(ECHO) "Done"
+
+clean:
+	@$(ECHO) -n "Cleaning up..."
+	@$(RM) -rf $(OBJDIR) *~ $(DEPS) $(SRCDIR)/*~ $(BINDIR) $(LIBDIR)
+	@$(ECHO) "Done"
+
+distclean: clean
+	@$(ECHO) -n "Removing Box2D Install..."
+	@$(RM) -rf $(BOX2D_ROOT)/include/* $(BOX2D_ROOT)/lib/* $(BOX2D_ROOT)/src/Box2D
+	@$(ECHO) "Box2D Removed"
+
+report: 
+	@cd doc; \
+	pdflatex cs296_report_19.tex; \
+	pdflatex cs296_report_19.tex; \
+	bibtex cs296_report_19; \
+	pdflatex cs296_report_19.tex; \
+	pdflatex cs296_report_19.tex;
---
+++ src/dominos.cpp
@@ -0,0 +1,459 @@
+/*
+* Copyright (c) 2006-2009 Erin Catto http://www.box2d.org
+*
+* This software is provided 'as-is', without any express or implied
+* warranty.  In no event will the authors be held liable for any damages
+* arising from the use of this software.
+* Permission is granted to anyone to use this software for any purpose,
+* including commercial applications, and to alter it and redistribute it
+* freely, subject to the following restrictions:
+* 1. The origin of this software must not be misrepresented; you must not
+* claim that you wrote the original software. If you use this software
+* in a product, an acknowledgment in the product documentation would be
+* appreciated but is not required.
+* 2. Altered source versions must be plainly marked as such, and must not be
+* misrepresented as being the original software.
+* 3. This notice may not be removed or altered from any source distribution.
+*/
+
+/* 
+ * Base code for CS 296 Software Systems Lab 
+ * Department of Computer Science and Engineering, IIT Bombay
+ * Instructor: Parag Chaudhuri
+ */
+
+
+#include "cs296_base.hpp"
+#include "render.hpp"
+
+#ifdef __APPLE__
+	#include <GLUT/glut.h>
+#else
+	#include "GL/freeglut.h"
+#endif
+
+#include <cstring>
+using namespace std;
+
+#include "dominos.hpp"
+
+namespace cs296
+{
+  /**  This is the constructor \n
+   * This is the documentation block for the constructor.
+   */
+  dominos_t::dominos_t()
+  {
+    //Ground
+   /** The ground has an EdgeShape between co-ordinates (-90, 0) and (90, 0).
+     * 
+     */ 
+    b2Body* b1;
+    {
+      
+      b2EdgeShape shape; 
+      shape.Set(b2Vec2(-90.0f, 0.0f), b2Vec2(90.0f, 0.0f));
+      b2BodyDef bd; 
+      b1 = m_world->CreateBody(&bd); 
+      b1->CreateFixture(&shape, 0.0f);
+    }      
+    //Top horizontal shelf
+    /** The top horizontal shelf has a PolygonShape of dimensions (6,0.25).\n
+     * The center of the shelf is at (-31, 30).
+     *
+     */
+    {
+      b2PolygonShape shape;
+      shape.SetAsBox(6.0f, 0.25f);
+	
+      b2BodyDef bd;
+      bd.position.Set(-31.0f, 30.0f);
+      b2Body* ground = m_world->CreateBody(&bd);
+      ground->CreateFixture(&shape, 0.0f);
+    }
+
+    //Dominos
+    /** The Dominos have PolygonShape of dimensions (0.2, 2.0). \n
+     * The center of the ith domino is at (-35.5 + i, 31.25). \n
+     * There are 10 dominos. \n
+     * They have a density of 20.
+     *
+     */
+    {
+      b2PolygonShape shape;
+      shape.SetAsBox(0.1f, 1.0f);
+	
+      b2FixtureDef fd;
+      fd.shape = &shape;
+      fd.density = 20.0f;
+      fd.friction = 0.1f;
+		
+      for (int i = 0; i < 10; ++i)
+	{
+	  b2BodyDef bd;
+	  bd.type = b2_dynamicBody;
+	  bd.position.Set(-35.5f + 1.0f * i, 31.25f);
+	  b2Body* body = m_world->CreateBody(&bd);
+	  body->CreateFixture(&fd);
+	}
+    }
+      
+    //Another horizontal shelf
+    /** The second horizontal shelf has a PolygonShape of dimensions (14, 0.5). \n
+     * Its center is located at (-20, 20).
+     *
+     */
+    {
+      b2PolygonShape shape;
+      shape.SetAsBox(7.0f, 0.25f, b2Vec2(-20.f,20.f), 0.0f);
+	
+      b2BodyDef bd;
+      bd.position.Set(1.0f, 6.0f);
+      b2Body* ground = m_world->CreateBody(&bd);
+      ground->CreateFixture(&shape, 0.0f);
+    }
+    //The long box on the left
+    /** The Long Box on the left has a PolygonShape SetAsBox of size (1, 16).\n
+     * It's position is (-40, 8).\n
+     * It's velocity is (0, 20).\n
+     * It's angular velocity is -1.\n
+     * It's density is 0.01
+     *
+     */
+    {
+      b2PolygonShape shape2;
+      shape2.SetAsBox(1.0f, 16.0f);
+      b2BodyDef bd3;
+      bd3.position.Set(-40.0f, 8.0f);
+      bd3.type = b2_dynamicBody;
+      bd3.angularVelocity = -1.0f;
+      bd3.linearVelocity.Set(0.0f, 20.0f);
+      b2Body* body3 = m_world->CreateBody(&bd3);
+      b2FixtureDef *fd3 = new b2FixtureDef;
+      fd3->density = 0.01f;
+      fd3->shape = new b2PolygonShape;
+      fd3->shape = &shape2;
+      body3->CreateFixture(fd3);
+    }
+
+    //The Ball Hitting the Long Box
+    /** The ball has a CircleShape of radius 1. \n
+     *  It's position is (-50,10). \n
+     *  It has a linear velocity of (30,10). \n
+     *  It has a density of 2.
+     *
+     */
+  
+    {
+    b2Body* spherebody;	
+      b2CircleShape circle;
+      circle.m_radius = 1.0;
+      b2FixtureDef ballfd;
+      ballfd.shape = &circle;
+      ballfd.density = 2.0f;
+      ballfd.friction = 0.0f;
+      ballfd.restitution = 0.0f;
+	  b2BodyDef ballbd;
+	  ballbd.linearVelocity.Set(30.0f, 10.0f);
+	  ballbd.type = b2_dynamicBody;
+	  ballbd.position.Set(-50.0f, 10.0f);
+	  spherebody = m_world->CreateBody(&ballbd);
+	  spherebody->CreateFixture(&ballfd);
+	
+    }
+    {
+	//Projectile Hitting The Bob
+    /** The projectile has a CircleShape of radius 1. \n
+     *  It's position is (-50,50). \n
+     *  It has a linear velocity of (30,10). \n
+     *  It has a density of 2.
+     *
+     */
+      b2Body* spherebody;
+	
+      b2CircleShape circle;
+      circle.m_radius = 0.5;
+      b2FixtureDef ballfd;
+      ballfd.shape = &circle;
+      ballfd.density = 0.2f;
+      ballfd.friction = 0.0f;
+      ballfd.restitution = 0.0f;
+	  b2BodyDef ballbd;
+	  ballbd.linearVelocity.Set(40.0f, 0.0f);
+	  ballbd.type = b2_dynamicBody;
+	  ballbd.position.Set(-40.0f, 32.0f);
+	  spherebody = m_world->CreateBody(&ballbd);
+	  spherebody->CreateFixture(&ballfd);
+	
+    }
+    //The pendulum that knocks the dominos off
+    /** The pendulum system consists of a revolute joint between a bob and a static body. \n
+     * The static body has a PolygonShape of dimensions (0.25,1.5) and is centered at (-36.5,28). \n
+     * The bob has a PolygonShape of dimensions (0.25,0.25), is centered at (-40,33) and has a density of 2. \n
+     * The anchor is set at (-37,40). 
+     * 
+     */
+    {
+      b2Body* b2;
+      {
+	b2PolygonShape shape;
+	shape.SetAsBox(0.25f, 1.5f);
+	  
+	b2BodyDef bd;
+	bd.position.Set(-36.5f, 29.0f);
+	b2 = m_world->CreateBody(&bd);
+	b2->CreateFixture(&shape, 10.0f);
+      }
+
+    
+      b2Body* b4;
+      {
+	b2PolygonShape shape;
+	shape.SetAsBox(0.5f, 0.5f);
+	  
+	b2BodyDef bd;
+	bd.type = b2_dynamicBody;
+	bd.position.Set(-37.0f, 32.0f);
+	b4 = m_world->CreateBody(&bd);
+	b4->CreateFixture(&shape, 2.0f);
+      }
+	
+      b2RevoluteJointDef jd;
+      b2Vec2 anchor;
+      anchor.Set(-37.0f, 40.0f);
+      jd.Initialize(b2, b4, anchor);
+      m_world->CreateJoint(&jd);
+    }
+  
+    //The train of small spheres
+    /** The train of spheres have a CircleShape of radius 0.5. \n
+     *  They have densities of 1. \n
+     *  There are 10 spheres. \n
+     *  The ith sphere has a position of (-22.2+i,26.6).
+     *
+     */
+    {
+      b2Body* spherebody;
+	
+      b2CircleShape circle;
+      circle.m_radius = 0.5;
+	
+      b2FixtureDef ballfd;
+      ballfd.shape = &circle;
+      ballfd.density = 1.0f;
+      ballfd.friction = 0.0f;
+      ballfd.restitution = 0.0f;
+	
+      for (int i = 0; i < 10; ++i)
+	{
+	  b2BodyDef ballbd;
+	  ballbd.type = b2_dynamicBody;
+	  ballbd.position.Set(-22.2f + i*1.0, 26.6f);
+	  spherebody = m_world->CreateBody(&ballbd);
+	  spherebody->CreateFixture(&ballfd);
+	}
+    }
+
+    //The pulley system
+    /** The pulley system consists of an open box, a bar and a pulley joint. 
+     */
+
+    {
+      b2BodyDef *bd = new b2BodyDef;
+      bd->type = b2_dynamicBody;
+      bd->position.Set(-10,15);
+      bd->fixedRotation = true;
+      
+      //The open box
+      /** The open box is made up of 3 polygonShape and is centered at (-10,15). \n
+       *  One of them has a size of (2,0.2) and have centers are located at (0,-1.9). \n
+       *  The other two have sizes of (2,0.2) and centers located at (2,0) and (-2,0) respectively. \n
+       *  It has the property of fixed rotation and a density of 10.
+       * 
+       */
+      b2FixtureDef *fd1 = new b2FixtureDef;
+      fd1->density = 10.0;
+      fd1->friction = 0.5;
+      fd1->restitution = 0.f;
+      fd1->shape = new b2PolygonShape;
+      b2PolygonShape bs1;
+      bs1.SetAsBox(2,0.2, b2Vec2(0.f,-1.9f), 0);
+      fd1->shape = &bs1;
+      b2FixtureDef *fd2 = new b2FixtureDef;
+      fd2->density = 10.0;
+      fd2->friction = 0.5;
+      fd2->restitution = 0.f;
+      fd2->shape = new b2PolygonShape;
+      b2PolygonShape bs2;
+      bs2.SetAsBox(0.2,2, b2Vec2(2.0f,0.f), 0);
+      fd2->shape = &bs2;
+      b2FixtureDef *fd3 = new b2FixtureDef;
+      fd3->density = 10.0;
+      fd3->friction = 0.5;
+      fd3->restitution = 0.f;
+      fd3->shape = new b2PolygonShape;
+      b2PolygonShape bs3;
+      bs3.SetAsBox(0.2,2, b2Vec2(-2.0f,0.f), 0);
+      fd3->shape = &bs3;
+       
+      b2Body* box1 = m_world->CreateBody(bd);
+      box1->CreateFixture(fd1);
+      box1->CreateFixture(fd2);
+      box1->CreateFixture(fd3);
+
+      //The bar
+      /** The bar has its center at (10,15). \n
+       * Its density is 34. 
+       * 
+       */
+      bd->position.Set(10,15);	
+      fd1->density = 34.0;	  
+      b2Body* box2 = m_world->CreateBody(bd);
+      box2->CreateFixture(fd1);
+
+      // The pulley joint
+      /** The pulley joint consists of anchor points at (-10,15), (10,15), (10,20) and (10,20). \n
+       * The length ratio is 1.
+       * 
+       */
+      b2PulleyJointDef* myjoint = new b2PulleyJointDef();
+      b2Vec2 worldAnchorOnBody1(-10, 15); // Anchor point on body 1 in world axis
+      b2Vec2 worldAnchorOnBody2(10, 15); // Anchor point on body 2 in world axis
+      b2Vec2 worldAnchorGround1(-10, 20); // Anchor point for ground 1 in world axis
+      b2Vec2 worldAnchorGround2(10, 20); // Anchor point for ground 2 in world axis
+      float32 ratio = 1.0f; // Define ratio
+      myjoint->Initialize(box1, box2, worldAnchorGround1, worldAnchorGround2, box1->GetWorldCenter(), box2->GetWorldCenter(), ratio);
+      m_world->CreateJoint(myjoint);
+    }
+
+    //The revolving horizontal platform
+    /** The revolving horizontal platform has a polygonShape of dimensions (2.2,0.2). \n
+     *  Its center is located at (14,14). \n
+     *  It has a density of 1. \n
+     *  It is connected to a hidden static body located at (14,16) via a revolute joint.
+     * 
+     */
+    {
+      b2PolygonShape shape;
+      shape.SetAsBox(2.2f, 0.2f);
+	
+      b2BodyDef bd;
+      bd.position.Set(14.0f, 14.0f);
+      bd.type = b2_dynamicBody;
+      b2Body* body = m_world->CreateBody(&bd);
+      b2FixtureDef *fd = new b2FixtureDef;
+      fd->density = 1.f;
+      fd->shape = new b2PolygonShape;
+      fd->shape = &shape;
+      body->CreateFixture(fd);
+
+      b2PolygonShape shape2;
+      shape2.SetAsBox(0.2f, 2.0f);
+      b2BodyDef bd2;
+      bd2.position.Set(14.0f, 16.0f);
+      b2Body* body2 = m_world->CreateBody(&bd2);
+
+      b2RevoluteJointDef jointDef;
+      jointDef.bodyA = body;
+      jointDef.bodyB = body2;
+      jointDef.localAnchorA.Set(0,0);
+      jointDef.localAnchorB.Set(0,0);
+      jointDef.collideConnected = false;
+      m_world->CreateJoint(&jointDef);
+    }
+
+    //The heavy sphere on the platform
+    /** The heavy sphere on the platform has a CircleShape of radius 1. \n
+     * Its center is located at (14,18) and it has a density of 50.
+     * 
+     */
+    {
+      b2Body* sbody;
+      b2CircleShape circle;
+      circle.m_radius = 1.0;
+	
+      b2FixtureDef ballfd;
+      ballfd.shape = &circle;
+      ballfd.density = 500.0f;
+      ballfd.friction = 0.0f;
+      ballfd.restitution = 0.0f;
+      b2BodyDef ballbd;
+      ballbd.type = b2_dynamicBody;
+      ballbd.position.Set(14.0f, 18.0f);
+      sbody = m_world->CreateBody(&ballbd);
+      sbody->CreateFixture(&ballfd);
+    }
+
+
+    //The see-saw system at the bottom
+    /** The see-saw is made up of a revolute joint between a static triangle wedge and a plank on top of it. \n
+     * The joint is anchored at (30,1.5).
+     * 
+     */
+    {
+      //The triangle wedge
+      /** The triangular wedge has a PolygonShape consisting of 3 vertices located at (-1,0), (1,0) and (0,1.5). \n
+       * It has a density of 10 and center located at (30,0).
+       * 
+       */
+      b2Body* sbody;
+      b2PolygonShape poly;
+      b2Vec2 vertices[3];
+      vertices[0].Set(-1,0);
+      vertices[1].Set(1,0);
+      vertices[2].Set(0,1.5);
+      poly.Set(vertices, 3);
+      b2FixtureDef wedgefd;
+      wedgefd.shape = &poly;
+      wedgefd.density = 10.0f;
+      wedgefd.friction = 0.0f;
+      wedgefd.restitution = 0.0f;
+      b2BodyDef wedgebd;
+      wedgebd.position.Set(30.0f, 0.0f);
+      sbody = m_world->CreateBody(&wedgebd);
+      sbody->CreateFixture(&wedgefd);
+
+      //The plank on top of the wedge
+      /** The plank has a PolygonShape of dimensions (15,0.2) and center at (30,1.5). \n
+       * It has a density of 1.
+       * 
+       */
+      b2PolygonShape shape;
+      shape.SetAsBox(15.0f, 0.2f);
+      b2BodyDef bd2;
+      bd2.position.Set(30.0f, 1.5f);
+      bd2.type = b2_dynamicBody;
+      b2Body* body = m_world->CreateBody(&bd2);
+      b2FixtureDef *fd2 = new b2FixtureDef;
+      fd2->density = 1.f;
+      fd2->shape = new b2PolygonShape;
+      fd2->shape = &shape;
+      body->CreateFixture(fd2);
+
+      b2RevoluteJointDef jd;
+      b2Vec2 anchor;
+      anchor.Set(30.0f, 1.5f);
+      jd.Initialize(sbody, body, anchor);
+      m_world->CreateJoint(&jd);
+
+      //The light box on the right side of the see-saw
+      /** The light box on the see-saw has a PolygonShape of dimensions (2,2) and is centered at (40,2). \n
+       * It has a density of 0.01.
+       * 
+       */
+      b2PolygonShape shape2;
+      shape2.SetAsBox(2.0f, 2.0f);
+      b2BodyDef bd3;
+      bd3.position.Set(40.0f, 2.0f);
+      bd3.type = b2_dynamicBody;
+      b2Body* body3 = m_world->CreateBody(&bd3);
+      b2FixtureDef *fd3 = new b2FixtureDef;
+      fd3->density = 0.001f;
+      fd3->shape = new b2PolygonShape;
+      fd3->shape = &shape2;
+      body3->CreateFixture(fd3);
+    }
+  }
+
+  sim_t *sim = new sim_t("Dominos", dominos_t::create);
+}
